ab786ff4c6650721a6d7cedb15bd42aedd33917b
N0GN0l <danielzhou323@gmail.com>
2023-06-10 18:23:43 -0400

    creation

--- creation.py
+++ creation.py
@@ -1,7 +1,8 @@
 numbers = int(input("How many numbers would you like to detect up to?: "))
-for i in range(numbers):
-    print("if num == " + str(i) )
+print('num = input("What is ur number?: ")')
+for i in range(numbers + 1):
+    print("if num == " + str(i) + ":")
     if (i % 2) == 0 :
-        print("\tprint(Your number is even)")
+        print('\tprint("Your number is even")')
     else:
-        print("\tprint(Your number is odd)")
+        print('\tprint("Your number is odd")')
