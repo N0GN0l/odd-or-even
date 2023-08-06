fad0b14e975fccfe0ae02074831956751a94640b
N0GN0l <danielzhou323@gmail.com>
2023-08-05 23:05:14 -0400

    Crates a text file instead of dumping into console now

--- creation.py
+++ creation.py
@@ -1,8 +1,12 @@
 numbers = int(input("How many numbers would you like to detect up to?: "))
-print('num = input("What is ur number?: ")')
-for i in range(numbers + 1):
-    print("if num == " + str(i) + ":")
-    if (i % 2) == 0 :
-        print('\tprint("Your number is even")')
-    else:
-        print('\tprint("Your number is odd")')
+with open('effective_detector.txt', 'w') as effective:
+    effective.write('num = input("What is ur number?: ")\n')    
+    for i in range(numbers + 1):
+        effective.write("if num == " + str(i) + ":\n" )
+        if (i % 2) == 0 :
+            effective.write('\tprint("Your number is even")\n')
+        else:
+            effective.write('\tprint("Your number is odd")\n')
+
+
+
